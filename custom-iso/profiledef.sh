593cee9a89820408e4055e535ee6d5bac089d217
apehex <apehex@protonmail.com>
2022-02-19 21:36:35 +0100

    Add the user archie

--- custom-iso/profiledef.sh
+++ custom-iso/profiledef.sh
@@ -10,3 +10,10 @@ install_dir="blackarch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+
+file_permissions=(
+  ["/etc/shadow"]="0:0:0400"
+  ["/etc/gshadow"]="0:0:0400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+)
