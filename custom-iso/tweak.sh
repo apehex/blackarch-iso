21ac396237520efba2ceca620fe6b769ffdfcafc
apehex <apehex@protonmail.com>
2022-02-19 20:38:05 +0100

    Copy the base profile into the workdir

--- custom-iso/tweak.sh
+++ custom-iso/tweak.sh
@@ -2,18 +2,23 @@
 
 # init
 SOURCEDIR="`dirname $(realpath ${0})`"
-WORKDIR="`mktemp -d`"
+WORKDIR="`mktemp -d`/custom-iso"
 BUNDLE="${1:-slim-iso}"
 ARCH="x86_64"
 # _GROUPS="`pacman -Sg | grep blackarch- | sort -u | tr -s '\n' ' '`"
+
+# copy the target profile
+cp -r "${SOURCEDIR}/../${BUNDLE}/" "${WORKDIR}" 
 
 # discard packages
 grep -Fxvf "${SOURCEDIR}/packages.${ARCH}.discard" "${SOURCEDIR}/../${BUNDLE}/packages.${ARCH}" > "${WORKDIR}/packages.${ARCH}.tmp"
 
 # add packages
-cat "${SOURCEDIR}/packages.${ARCH}.add" "${WORKDIR}/packages.${ARCH}.tmp" > "${WORKDIR}/packages.${ARCH}"
+cat "${SOURCEDIR}/packages.${ARCH}.add" "${WORKDIR}/packages.${ARCH}.tmp" | sort -u | grep -v "###" > "${WORKDIR}/packages.${ARCH}"
 
-# remove comments
+# import dotfiles
+
+# clone tools
 
 # remove temp files
 rm "${WORKDIR}/packages.${ARCH}.tmp"
