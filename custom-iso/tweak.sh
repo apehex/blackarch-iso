35b6d80d4378caccbd73f7f46dc13acaca5af3a2
apehex <apehex@protonmail.com>
2022-02-20 18:22:00 +0100

    Tweak the post install hook script

--- custom-iso/tweak.sh
+++ custom-iso/tweak.sh
@@ -16,6 +16,29 @@ grep -Fxvf "${SOURCEDIR}/packages.${ARCH}.discard" "${SOURCEDIR}/../${BUNDLE}/pa
 # add packages
 cat "${SOURCEDIR}/packages.${ARCH}.add" "${WORKDIR}/packages.${ARCH}.tmp" | sort -u | grep -v "###" > "${WORKDIR}/packages.${ARCH}"
 
+# remove unwanted files
+rm -rf "${WORKDIR}/airootfs/etc/skel/*"
+
+# tweak post install hook
+perl -pi -e "s#'lightdm.service' ##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#'NetworkManager' ##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#'irqbalance' ##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#cp /usr/share/blackarch/config/bash/bashrc /etc/skel/.bashrc##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#cp /usr/share/blackarch/config/bash/bash_profile /etc/skel/.bash_profile##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#cp /usr/share/blackarch/config/zsh/zshrc /etc/skel/.zshrc##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#useradd -m -g users -G wheel,power,audio,video,storage -s /bin/zsh liveuser##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e 's#echo "liveuser:blackarch" | chpasswd##gi' "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#cp -r /usr/share/blackarch/config/vim/vim /home/liveuser/.vim##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#cp /usr/share/blackarch/config/vim/vimrc /home/liveuser/.vimrc##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#ln -sf /usr/share/icons/blackarch-icons/apps/scalable/distributor-logo-blackarch.svg /home/liveuser/.face##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#mkdir -p /home/liveuser/Desktop##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#chown -R liveuser:users /home/liveuser/Desktop##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#chmod -R 755 /home/liveuser/Desktop##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#ln -sf /usr/share/applications/calamares.desktop /home/liveuser/Desktop/calamares.desktop#ln -sf /usr/share/applications/calamares.desktop /home/archie/.local/bin/calamares.desktop#gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#ln -sf /usr/share/applications/xfce4-terminal-emulator.desktop /home/liveuser/Desktop/terminal.desktop##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#chmod +x /home/liveuser/Desktop/*.desktop##gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+perl -pi -e "s#liveuser#archie#gi" "${WORKDIR}/airootfs/root/customize_airootfs.sh"
+
 # adding files (user dir)
 rsync -avh --update --progress "${SOURCEDIR}/airootfs/" "${WORKDIR}/airootfs/"
 
