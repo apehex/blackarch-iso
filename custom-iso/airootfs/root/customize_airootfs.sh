e07e5445a0a2d112b44bf85494539a4e84ecb170
apehex <apehex@protonmail.com>
2022-03-04 22:45:12 +0100

    No need to configure GDK tools, Gnome is not installed

--- custom-iso/airootfs/root/customize_airootfs.sh
+++ custom-iso/airootfs/root/customize_airootfs.sh
@@ -78,6 +78,3 @@ cd /usr/share/whatweb && rm -f Gemfile.lock &&
 
 # Temporary fix for calamares
 pacman -U --noconfirm https://archive.archlinux.org/packages/d/dosfstools/dosfstools-4.1-3-x86_64.pkg.tar.xz
-
-# GDK Pixbuf
-gdk-pixbuf-query-loaders --update-cache
